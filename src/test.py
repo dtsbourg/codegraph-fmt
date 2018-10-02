d8c3417c7aa03730d1d7b0bdfc3de799e86bfe5f
dtsbourg <dtsbourg@gmail.com>
2018-10-02 15:42:48 -0700

    move to astor

--- src/test.py
+++ src/test.py
@@ -6,6 +6,7 @@ Utility to dump an AST from a set of files.
 License: CC-BY 4.0
 """
 
+import joblib
 import astpp
 import os
 from collections import namedtuple
@@ -18,11 +19,20 @@ path = os.path.join(cfg.datapath, cfg.name, 'raw')
 dumpdir = os.path.join(cfg.datapath, cfg.name, 'AST')
 # END CFG
 
+#####################################
+# Astor example
+####################################
+
+import astor
 
 for file in os.listdir(path):
     if file.endswith(".py"):
         f = os.path.join(path, file)
         print("Processing file", f)
-        with open(f, 'r') as myfile:
-           data=myfile.read()
-        astpp.pdp(data, dumpdir=dumpdir)
+        parsed_ast = astor.code_to_ast.parse_file(f)
+        ast_dump = astpp.dump(parsed_ast)
+        with open(os.path.join(dumpdir, 'AST-str-dump.txt'), 'w') as f:
+            f.write(ast_dump)
+        joblib.dump(parsed_ast, os.path.join(dumpdir, 'AST-bin-dump.ast'))
+        print(ast_dump)
+        print()
