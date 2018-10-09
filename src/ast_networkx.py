fdda24ef81c1ae2d5a90844a18d85cfcba381a05
Thao Nguyen <thachthao2605@gmail.com>
2018-10-09 00:38:03 -0700

    convert function

--- src/ast_networkx.py
+++ src/ast_networkx.py
@@ -4,28 +4,30 @@ import ast
 import json
 
 data_path = '/lfs/hyperion3/0/thaonguyen/SAGE-fmt/data/code-sample/AST/'
-tree = utils.load(data_path + 'AST-bin-dump.ast')
-all_nodes = list(ast.walk(tree)) # BFS traversal
-all_nodes_set = set(all_nodes)
+ast_path = data_path + 'AST-bin-dump.ast'
 
-DG = nx.DiGraph()
-id_map_dict = {}
-ast_id_mapping = {}
-curr_id = 0
-for node in all_nodes_set:
-    DG.add_node(curr_id)
-    ast_id_mapping[node] = curr_id
-    id_map_dict[str(curr_id)] = curr_id
-    curr_id += 1
+def generate_json(ast_path):
+    tree = utils.load(ast_path)
+    all_nodes = list(ast.walk(tree)) # BFS traversal
+    all_nodes_set = set(all_nodes)
 
-for node in all_nodes_set:
-    for child in ast.iter_child_nodes(node):
-        #DG.add_node(child)
-        DG.add_edge(ast_id_mapping[node], ast_id_mapping[child])
+    DG = nx.DiGraph()
+    id_map_dict = {}
+    ast_id_mapping = {}
+    curr_id = 0
+    for node in all_nodes_set:
+        DG.add_node(curr_id)
+        ast_id_mapping[node] = curr_id
+        id_map_dict[str(curr_id)] = curr_id
+        curr_id += 1
 
-#n_nodes = len(list(DG.nodes))
-with open(data_path + 'id_map.json', 'w') as fout:
-    fout.write(json.dumps(id_map_dict))
+    for node in all_nodes_set:
+        for child in ast.iter_child_nodes(node):
+            #DG.add_node(child)
+            DG.add_edge(ast_id_mapping[node], ast_id_mapping[child])
 
-with open(data_path + 'G.json', 'w') as fout:
-    fout.write(json.dumps(nx.json_graph.node_link_data(DG)))
+    #n_nodes = len(list(DG.nodes))
+    with open(data_path + 'id_map.json', 'w') as fout:
+        fout.write(json.dumps(id_map_dict))
+    with open(data_path + 'G.json', 'w') as fout:
+        fout.write(json.dumps(nx.json_graph.node_link_data(DG)))
