127b3b4248a40ed2072ad6e48201058dcfab96c5
dtsbourg <dtsbourg@gmail.com>
2018-11-15 15:52:04 -0800

    create file blacklist

--- src/project_crawler.py
+++ src/project_crawler.py
@@ -12,13 +12,20 @@ License: CC-BY 4.0
 
 import os
 
+blacklist = [
+    '__init__.py',
+    'setup.py',
+    'autogen.py'
+]
+
 def crawl(path, verbose=False, filetype='.py'):
     ps = [];
     for root, directories, filenames in os.walk(path):
         for filename in filenames:
             if filename.endswith(filetype):
-                p = os.path.join(root,filename)
-                ps.append(p)
+                if not filename in blacklist:
+                    p = os.path.join(root,filename)
+                    ps.append(p)
         if verbose:
             print("[CRAWL]  --- Processed {0} files in {1}".format(len(filenames), root))
     print()
